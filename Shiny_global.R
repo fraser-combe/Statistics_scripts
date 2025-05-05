3f5a2dda04531217703de935c03252e59f8e4fe3
Fraser Combe <frascom@hotmail.com>
2025-05-04 20:49:50 -0500

    Create Shiny_global.R

---
+++ Shiny_global.R
@@ -0,0 +1,35 @@
+#===============================================================================
+# This is the global.R script for the fastpas tree shiny app
+#
+# Fraser Combe
+#===============================================================================
+
+if (!"treeio" %in% installed.packages()) {
+  if (!"devtools" %in% installed.packages()) install.packages("devtools")
+  devtools::install_github("GuangchuangYu/treeio")
+} else if (packageVersion("treeio") < "1.5.1.2") {
+  if (!"devtools" %in% installed.packages()) install.packages("devtools")
+  devtools::install_github("GuangchuangYu/treeio")
+}
+
+if (!"purrr" %in% installed.packages()) install.packages("purrr")
+
+
+purrr::walk(c("shiny", "shinyjs", "tidyverse", 
+              "ggtree", "tidytree", "shinyalert"), ~{
+  if (!.x %in% installed.packages()) install.packages(.x)
+})
+  
+
+suppressWarnings({
+  suppressPackageStartupMessages({
+    library(shiny)
+    library(shinyjs)
+    library(tidyverse)
+    library(ggtree)
+    library(tidytree)
+    library(treeio)
+    library(shinyalert)
+  })
+})
+
